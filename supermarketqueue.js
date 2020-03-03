6640ac6d930d2bb0abc68d0d8d05652b154d59ce
Guru-Sandhu <arsh3874@gmail.com>
2020-03-03 09:22:04 -0800

    the supermarket queue

---
+++ supermarketqueue.js
@@ -0,0 +1,16 @@
+function queueTime(customers, n) {
+    let queue = []
+
+    for (let i = 0 ; i < n; i++) {
+        queue.push(0)
+    }
+
+    for (let num of customers) {
+        queue[0] += num;
+        queue.sort((a,b) => {
+            return a-b;
+        })
+    }
+
+    return Math.max(...queue)
+}
